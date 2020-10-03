0bc0d8855069dc07302a31426288adf696ed9d5a
derysudrajat <dery.sudrajat17@gmail.com>
2020-10-03 18:15:24 +0700

    add: getAlphaForActionBar and getNumEpisode in Helpers.kt

--- app/src/main/java/com/kaedenoki/moviecorner/helper/Helpers.kt
+++ app/src/main/java/com/kaedenoki/moviecorner/helper/Helpers.kt
@@ -15,4 +15,28 @@ object Helpers {
         val fix = this.split("E")
         return buildString { append(fix[0]).append(" E").append(fix[1]) }
     }
+
+    fun getAlphaForActionBar(scrollY: Int): Int {
+        val minDist = 0
+        val maxDist = 650
+        return when {
+            scrollY > maxDist -> {
+                255
+            }
+            scrollY < minDist -> {
+                0
+            }
+            else -> {
+                var alpha = 0
+                alpha = (255.0 / maxDist * scrollY).toInt()
+                alpha
+            }
+        }
+    }
+
+    fun String.getNumEpisode(): String {
+        val data = this.split("Episode ")
+        val episode = data[1].split(" ")
+        return episode[0]
+    }
 }
